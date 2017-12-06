2ed190db92239ce66d8447841fdeb78f002fe40c
Edgar Cruz <cruz085@cougars.csusm.edu>
2017-12-05 20:58:21 -0800

    commit

--- src/finance/loginPage.java
+++ src/finance/loginPage.java
@@ -114,6 +114,7 @@ PreparedStatement pst = null;
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
 private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
